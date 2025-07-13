28fc15187fa5398b23dbd359c3f306c090ae4786
Gagan Narang <gagannarang0804@gmail.com>
2025-07-13 15:49:05 +0530

    run file

--- run.py
+++ run.py
@@ -1,1 +1,11 @@
-.
+import subprocess
+import threading
+
+def run_fastapi():
+    subprocess.call(["uvicorn", "server:app", "--host", "0.0.0.0", "--port", "8000"])
+
+def run_streamlit():
+    subprocess.call(["streamlit", "run", "app.py", "--server.address=0.0.0.0"])
+
+threading.Thread(target=run_fastapi).start()
+run_streamlit()
